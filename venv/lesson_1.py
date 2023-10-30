1ca39e47bb3ed59d48b1f09e3fe456ff50abe0bc
Greensaver <greensaver.ua@gmail.com>
2023-10-30 22:05:46 +0200

    lesson_1

---
+++ venv/lesson_1.py
@@ -0,0 +1,24 @@
+print("Hello lesson_1")
+
+variable_1 = 20
+variable_2 = 5
+variable_3 = 3
+
+# print(variable_1 * variable_2)
+#
+# result = (variable_2-variable_1)
+# print(result)
+
+# # залишок від ділення %
+# result = variable_1 % variable_3  # 2
+# print(result)
+#
+# # скільки ми змогли поділити на число
+# result = variable_1 // variable_3 # 6
+#
+# #string
+# stringa = "TEST24"
+# print(type(stringa))
+
+age = int (input("How old are you "))
+print(type(age))
