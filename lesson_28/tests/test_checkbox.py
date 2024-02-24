4243e23ae89b369c8f5bd253febe52a91d47e5da
Greensaver <greensaver.ua@gmail.com>
2024-02-24 17:27:09 +0200

    lesson_28

---
+++ lesson_28/tests/test_checkbox.py
@@ -0,0 +1,18 @@
+import pytest
+from lesson_28.src.pages.CheckboxPage import CheckboxPage
+from lesson_28.conftest import chrome_class
+
+
+@pytest.mark.usefixtures("chrome_class")
+class TestCheckboxPage:
+    def setup(self):
+        self.page = CheckboxPage(self.driver)
+
+    def test_page(self):
+        self.page.open()
+        self.page.expand_folder("home")
+        self.page.expand_folder("desktop")
+        self.page.mark_folder("commands")
+        self.page.expand_folder("documents")
+        self.page.mark_folder("office")
+        pass
